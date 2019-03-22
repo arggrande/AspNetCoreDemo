dcdfa1a97612a1a6b9ecc20bf820c7ec939bf4d4
Allen Azemia <allen.azemia@health.telstra.com>
2019-03-22 16:24:18 +0800

    Added Utf8 Reader/Writer comparison speed performance

---
+++ AspNetCoreDemo/AspNetCoreDemo.Console/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AspNetCoreDemo.Console
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public string[] Cast { get; set; }
+
+        public string[] Genres { get; set; }
+    }
+
+}
---
+++ AspNetCoreDemo/AspNetCoreDemo.Console/Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AspNetCoreDemo.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var filePath = @"D:\Projects\VS2019LaunchParty\AspNetCoreDemo\Dataset\movies.json";
+            var moviesDataset = await File.ReadAllTextAsync(filePath);
+            
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var movies = MovieJsonConvert.Deserialize(moviesDataset);
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            System.Console.WriteLine("Utf8 Elapsed time: {0}", elapsedMs);
+
+            watch = System.Diagnostics.Stopwatch.StartNew();
+            movies = JsonConvert.DeserializeObject<IEnumerable<Movie>>(moviesDataset);
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            System.Console.WriteLine("Utf16 Elapsed time: {0}", elapsedMs);
+
+            System.Console.WriteLine("Writing Utf8 Json");
+            watch = System.Diagnostics.Stopwatch.StartNew();
+            var serializedMovies = MovieJsonConvert.Serialize(movies);
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            System.Console.WriteLine("Utf8 Elapsed time: {0}", elapsedMs);
+
+            System.Console.WriteLine("Writing Normal Json");
+            watch = System.Diagnostics.Stopwatch.StartNew();
+            serializedMovies = JsonConvert.SerializeObject(movies);
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            System.Console.WriteLine("Utf16 Elapsed time: {0}", elapsedMs);
+
+            System.Console.WriteLine("Movies loaded");
+            System.Console.ReadLine();
+        }
+    }
+}
